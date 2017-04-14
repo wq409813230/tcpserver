b42bb9dbf0a193f346cecffdcaf2219856ab04b6
wuqiang <wuqiang@zhaotai.net>
2017-04-14 11:20:44 +0800

    tcp learning project initialization!

---
+++ src/main/java/net/freeapis/io/TelnetServer.java
@@ -0,0 +1,18 @@
+package net.freeapis.io;
+
+/**
+ * freeapis, Inc.
+ * Copyright (C): 2016
+ * <p>
+ * Description:
+ * TODO
+ * <p>
+ * Author:Administrator
+ * Date:2017年04月14日 11:19
+ */
+public class TelnetServer {
+
+    public static void main(String[] args) {
+        
+    }
+}
