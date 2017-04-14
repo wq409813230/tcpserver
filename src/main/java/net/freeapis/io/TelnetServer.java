7b2959168a61c3c7e46bf266d82648e76655f371
wuqiang <wuqiang@zhaotai.net>
2017-04-14 17:17:23 +0800

    telnet server implemented by nio!

--- src/main/java/net/freeapis/io/TelnetServer.java
+++ src/main/java/net/freeapis/io/TelnetServer.java
@@ -1,5 +1,16 @@
 package net.freeapis.io;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * freeapis, Inc.
  * Copyright (C): 2016
@@ -12,7 +23,73 @@ package net.freeapis.io;
  */
 public class TelnetServer {
 
+    private static final int TCP_PORT = 9527;
+
+    private static final int SELECT_TIMEOUT = 3000;
+
+    private static final int DEFAULT_BUFFER_SIZE = 128;
+
     public static void main(String[] args) {
-        
+        Selector selector = null;
+        ServerSocketChannel serverSocketChannel = null;
+
+        try{
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(TCP_PORT));
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            while(true){
+                if(selector.select(SELECT_TIMEOUT) != 0){
+                    Iterator<SelectionKey> keysIter = selector.selectedKeys().iterator();
+                    while (keysIter.hasNext()){
+                        SelectionKey key = keysIter.next();
+                        if(key.isAcceptable()){
+                            accept(key);
+                        }
+                        if(key.isReadable()){
+                            read(key);
+                        }
+                        if(key.isConnectable()){
+                            System.out.println("connected");
+                        }
+                        keysIter.remove();
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            try {
+                if(selector != null) selector.close();
+                if(serverSocketChannel != null) serverSocketChannel.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void accept(SelectionKey key) throws IOException {
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        socketChannel.configureBlocking(false);
+        ByteBuffer channelBuffer = ByteBuffer.allocateDirect(DEFAULT_BUFFER_SIZE);
+        socketChannel.register(key.selector(),SelectionKey.OP_READ, channelBuffer);
+
+        channelBuffer.put("welcome to simple telnet server!".getBytes());
+        channelBuffer.flip();
+        socketChannel.write(channelBuffer);
+        channelBuffer.clear();
+    }
+
+    private static void read(SelectionKey key) throws IOException{
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        ByteBuffer channelBuffer = (ByteBuffer)key.attachment();
+        while(socketChannel.read(channelBuffer) > 0){
+            channelBuffer.flip();
+            socketChannel.write(channelBuffer);
+            channelBuffer.clear();
+        }
     }
 }
